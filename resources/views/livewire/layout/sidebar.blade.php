9432b58702996af8c906321b646b317e45e61d3c
rezasett <reza.sett@gmail.com>
2026-01-05 16:55:23 +0700

    update ijal

--- resources/views/livewire/layout/sidebar.blade.php
+++ resources/views/livewire/layout/sidebar.blade.php
@@ -88,8 +88,6 @@
             <div x-show="open === 'setting'" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100" x-transition:leave="transition ease-in duration-150" x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95" class="absolute left-0 w-52 mt-2 bg-white border rounded shadow-lg z-20" x-cloak>
               <ul>
 
-                
-                <li @click="open = null"><a href="#" class="block rounded-md py-2 pl-2 text-sm text-blue-900 hover:bg-blue-100">My Profile</a></li>
                 <li @click="open = null"><a href="#" class="block rounded-md py-2 pl-2 text-sm text-blue-900 hover:bg-blue-100">Setting</a></li>
               </ul>
             </div>
